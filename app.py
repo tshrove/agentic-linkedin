e7581172b1d883b06fad10e09030ed4c8255690f
Tommy Shrove <tshrove@gmail.com>
2025-01-28 21:36:23 -0600

    Updated with perplexity
    Updated with perplexity

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from crewai_tools import (
 )
 from crewai import LLM
 from tweepytwitterclient import TweepyTwitterClient
-from grok_tool import GrokTool
+# from grok_tool import GrokTool
+from perplexity_tool import PerplexityTool
 
 # Load environment variables from .env file
 load_dotenv('.env')
@@ -18,19 +19,20 @@ llm = LLM(
 )
 
 # Instantiate tools
-docs_tool = DirectoryReadTool(directory='./blog-posts')
+docs_tool = DirectoryReadTool(directory='./linkedin-posts')
 file_tool = FileReadTool()
-grok_tool = GrokTool()
+perplexity_tool = PerplexityTool()
 web_rag_tool = WebsiteSearchTool()
 
 # Create agents
 researcher = Agent(
     llm=llm,
     role='AI Research Analyst',
-    goal='Provide the latest news from today of the AI industry',
-    backstory='An expert analyst that loves to stay up-to-date with the latest AI news.',
-    tools=[grok_tool, web_rag_tool],
-    verbose=True
+    goal='Find the latest news from today in the Artificial Intelligence, AI, and Machine Learning industry.',
+    backstory='A Ph.D. in AI that loves to stay up-to-date with the latest AI news, trends, and developments.',
+    tools=[perplexity_tool],
+    verbose=True,
+    function_calling_llm=llm
 )
 
 writer = Agent(
@@ -45,17 +47,19 @@ writer = Agent(
 # Define tasks
 research = Task(
     llm=llm,
-    description='Research and find the latest news from today in Artificial Intelligence, AI, and Machine Learning and provide a summary.',
+    description='Find the latest news from today in the Artificial Intelligence, AI, and Machine Learning industry.',
     expected_output='A summary of the top 5 latest developments from today in the Artificial Intelligence, AI, and Machine Learning industry and why it is important.',
     agent=researcher,
-    verbose=True
+    verbose=True,
+    output_file='./linkedin-posts/research_summary.txt'
 )
 
 write = Task(
     llm=llm,
-    description="Write an engaging LinkedIn post about the latest AI news from today, based on the research analyst’s summary. Draw inspiration from the latest blog posts in the directory.",
+    description="Write an engaging LinkedIn post about the latest AI news from today, based on the research analyst’s summary. Draw inspiration from the latest news in the directory.",
     expected_output='A short LinkedIn post formatted in with engaging, informative, and accessible content.',
-    agent=writer
+    agent=writer,
+    output_file='linkedin_post.txt',
 )
 
 # Assemble a crew with planning enabled
--- tweepytwitterclient.py
+++ tweepytwitterclient.py
@@ -4,7 +4,7 @@ import tweepy
 
 class TweepyTwitterClient:
     def __init__(self):
-        # bearer_token = os.environ.get("BEARER_TOKEN")
+        bearer_token = os.environ.get("X_BEARER_TOKEN")
         consumer_api_key = os.environ.get("X_CONSUMER_API_KEY")
         consumer_api_key_secret = os.environ.get("X_CONSUMER_API_KEY_SECRET")
         access_token = os.environ.get("X_ACCESS_TOKEN")
@@ -13,6 +13,7 @@ class TweepyTwitterClient:
         # Authenticate with the Twitter API
         # _auth = tweepy.OAuth2BearerHandler(bearer_token=bearer_token)
         self.client = tweepy.Client(
+            bearer_token=bearer_token,
             consumer_key=consumer_api_key,
             consumer_secret=consumer_api_key_secret,
             access_token=access_token,
